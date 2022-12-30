730baf4a0925c2f7438f7b3fc11aab6b1ad1f681
Luka Dusak <lux.9833@gmail.com>
2022-12-30 22:44:58 +0100

    lab 06: files

---
+++ Part06_Files/src/hr/java/covidportal/model/ImenovaniEntitet.java
@@ -0,0 +1,38 @@
+package hr.java.covidportal.model;
+
+import java.io.Serializable;
+
+/**
+ * Sadrži dva svojstva, naziv i id, koji će u klasama koje naslijeđuju ovu klasu biti inicijalizirani.
+ */
+public abstract class ImenovaniEntitet implements Serializable {
+    private Long id;
+    private String naziv;
+
+    /**
+     * Prima id i naziv koji je potreban za inicijalizaciju budućeg entiteta.
+     *
+     * @param id id budućeg entiteta
+     * @param naziv ime budućeg entiteta
+     */
+    public ImenovaniEntitet(Long id, String naziv) {
+        this.id = id;
+        this.naziv = naziv;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+}
---
+++ Part06_Files/src/hr/java/covidportal/sort/VirusiKlinikaSorter.java
@@ -0,0 +1,25 @@
+package hr.java.covidportal.sort;
+
+import hr.java.covidportal.genericsi.KlinikaZaInfektivneBolesti;
+import hr.java.covidportal.model.Virus;
+
+import java.util.Comparator;
+
+/**
+ * Sortira viruse po nazivu obrnutom od abecednog.
+ */
+public class VirusiKlinikaSorter implements Comparator<Virus> {
+    @Override
+    public int compare(Virus virus1, Virus virus2) {
+        int usporedba = virus1.getNaziv().compareTo(virus2.getNaziv());
+        if(usporedba < 0){
+            return 1;
+        }
+        if(usporedba > 0){
+            return -1;
+        }
+        else {
+            return 0;
+        }
+    }
+}
